7cd5a960b1e757080a6514758ed5ac83301837b2
pagar22 <aaryanpagar@hotmail.com>
2022-04-12 17:59:19 +0200

    Initial Commit

---
+++ src/GlowCard.js
@@ -0,0 +1,11 @@
+import {Component} from "react";
+
+class GlowCard extends Component {
+    render() {
+        return (
+            <div className={'card'}>Glow Card</div>
+        )
+    }
+}
+
+export default GlowCard;
