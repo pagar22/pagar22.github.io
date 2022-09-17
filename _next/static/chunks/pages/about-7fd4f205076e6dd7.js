a18ce86fdd7f7ea5e3a7af4f5f914146438bece8
pagar22 <77506813+pagar22@users.noreply.github.com>
2022-09-17 23:44:43 +0000

    Deploying to gh-pages from @ pagar22/pagar22.github.io@b2f5766891ebb11c131113d2ff1e57703b64b733 🚀

--- _next/static/chunks/pages/about-7fd4f205076e6dd7.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[521],{9212:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about",function(){return t(2144)}])},2144:function(n,u,t){"use strict";t.r(u);var c=t(5893),r=function(){return(0,c.jsx)("h1",{children:"Helloo"})};u.default=r}},function(n){n.O(0,[774,888,179],function(){return n(n.s=9212)}),_N_E=n.O()}]);
