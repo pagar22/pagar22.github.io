979e8f863ee72f3d2737f04e43b9cf70b6a3b10a
pagar22 <aaryanpagar@hotmail.com>
2022-09-18 02:20:58 +0200

    feat: tailwind and basic pages

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
   return (
-    <h1>Helloo</h1>
+    <main className={styles.main}>
+      <h1 className={styles.title}><span className={"text-indigo-500"}>Shuuuush,</span> there's more coming soon.</h1>
+    </main>
   )
 }
 
