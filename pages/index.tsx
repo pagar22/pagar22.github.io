b62c12c25834513e0b713d7342479c5ab735da05
pagar22 <aaryanpagar@hotmail.com>
2022-09-24 17:54:49 +0200

    config: lint settings and favicon update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,34 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.scss'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.scss";
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Aaryan Pagar</title>
+        <link rel="icon" type="image/x-icon" href="ramen.ico" />
         <meta name="description" content="Aaryan Pagar's Personal Portfolio" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Hello, I&apos;m <span className={"text-indigo-500"}>
-          <a href="https://www.linkedin.com/in/pagar22/" target={"_blank"} rel="noreferrer">Aaryan</a></span>!</h1>
+        <h1 className={styles.title}>
+          Hello, I&apos;m{" "}
+          <span className={"text-indigo-500"}>
+            <a
+              href="https://www.linkedin.com/in/pagar22/"
+              target={"_blank"}
+              rel="noreferrer"
+            >
+              Aaryan
+            </a>
+          </span>
+          !
+        </h1>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
